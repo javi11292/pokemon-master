3c104419f1baaafcd07c78b2cc74458779252133
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-05-01 21:18:18 +0200

    Updates

--- _next/static/current/_buildManifest.js
+++ _next/static/current/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/404":[a],"/index":[a]}}("static\u002Fchunks\u002F489edbe52997a911566d1fa4f2937b29d13192ee.b9c4911d61b15d224fcd.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/404":[a],"/index":[a]}}("static\u002Fchunks\u002F489edbe52997a911566d1fa4f2937b29d13192ee.55275fc844213e136a30.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
