41d8df6e231370a5ba0b337d7a61eb9229fe6540
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-05-02 18:06:47 +0200

    Add assets

--- scripts/download.js
+++ scripts/download.js
@@ -2,12 +2,26 @@ const fetch = require("node-fetch")
 const fs = require("fs")
 const gameMaster = require("../game-master")
 
-const a = "https://db.pokemongohub.net/images/official/detail/002.png"
+function fetchVideos() {
+  return Object.keys(gameMaster.pokemons).map(async pokemon => {
+    const response = await fetch(`https://db.pokemongohub.net/videos/${pokemon}-small.mp4`)
+      .catch(() => console.log(`Missing ${pokemon}`))
+    response.body.pipe(fs.createWriteStream(`public/assets/videos/${pokemon}.mp4`))
+  })
+}
 
-Object.keys(gameMaster.pokemons).forEach(async pokemon => {
-  const interval = setInterval(() => console.log(`Getting ${pokemon}...`))
-  const response = await fetch(`https://db.pokemongohub.net/videos/${pokemon}-small.mp4`)
-  response.body.pipe(fs.createWriteStream(`public/assets/${pokemon}.mp4`))
-  console.log("END")
-  clearInterval(interval)
-})
+function fetchImages() {
+  return Object.entries(gameMaster.pokemons).map(async ([name, pokemon]) => {
+    const response = await fetch(`https://db.pokemongohub.net/images/official/detail/${pokemon.number.toString().padStart(3, "0")}.png`)
+      .catch(() => console.log(`Missing ${name}`))
+    response.body.pipe(fs.createWriteStream(`public/assets/images/${name}.png`))
+  })
+}
+
+async function run() {
+  await Promise.all(fetchVideos())
+  await Promise.all(fetchImages())
+  console.log("DONE")
+}
+
+run()
