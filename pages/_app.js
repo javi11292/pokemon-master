989f96be64cf026d98034f02c6ed9443961ad192
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-05-02 10:33:37 +0200

    Refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import Error from "next/error"
 import Main from "components/Main"
 
 export default function App({ Component, pageProps }) {
@@ -11,10 +10,7 @@ export default function App({ Component, pageProps }) {
       </Head>
 
       <Main>
-        {pageProps.statusCode
-          ? <Error statusCode={pageProps.statusCode} />
-          : <Component {...pageProps} />
-        }
+        <Component {...pageProps} />
       </Main>
     </>
   )
